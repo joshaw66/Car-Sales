a84a41f23d8bc83eea2fb392639f0e98c8a2faa9
Joshua Shaw <joshua@Joshuas-MacBook-Pro.local>
2020-04-28 20:50:05 -0600

    Making progress

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {useDispatch} from 'react-redux'
+import {buyFeature} from '../actions/action'
+
 
 const AdditionalFeature = props => {
+
+const dispatch = useDispatch();
+
+const handleAdd = () => {
+  buyFeature(props.feature);
+  dispatch(buyFeature(props.feature));
+};
+
   return (
     <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" type = 'submit' value={props.feature.id} onClick={handleAdd}>Add Car</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
